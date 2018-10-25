027139918959ab245a2ce8de8788a25b6764c280
outsung <40460655+outsung@users.noreply.github.com>
2018-10-26 04:36:03 +0900

    Add files via upload

--- pyimagesearch/centroidtracker.py
+++ pyimagesearch/centroidtracker.py
@@ -5,10 +5,9 @@ import numpy as np
 
 
 class CentroidTracker():
-
-    def __init__(self, maxDisappeared=50):
-        # initialize the next unique object ID along with two ordered
-        # dictionaries used to keep track of mapping a given object
+	def __init__(self, maxDisappeared=50):
+		# initialize the next unique object ID along with two ordered
+		# dictionaries used to keep track of mapping a given object
 		# ID to its centroid and number of consecutive frames it has
 		# been marked as "disappeared", respectively
 		self.nextObjectID = 0
@@ -20,20 +19,20 @@ class CentroidTracker():
 		# need to deregister the object from tracking
 		self.maxDisappeared = maxDisappeared
 
-    def register(self, centroid):
+	def register(self, centroid):
 		# when registering an object we use the next available object
 		# ID to store the centroid
 		self.objects[self.nextObjectID] = centroid
 		self.disappeared[self.nextObjectID] = 0
 		self.nextObjectID += 1
 
-    def deregister(self, objectID):
+	def deregister(self, objectID):
 		# to deregister an object ID we delete the object ID from
 		# both of our respective dictionaries
 		del self.objects[objectID]
 		del self.disappeared[objectID]
 
-    def update(self, rects):
+	def update(self, rects):
 		# check to see if the list of input bounding box rectangles
 		# is empty
 		if len(rects) == 0:
@@ -41,10 +40,6 @@ class CentroidTracker():
 			# as disappeared
 			for objectID in list(self.disappeared.keys()):
 				self.disappeared[objectID] += 1
-
-				# if we have reached a maximum number of consecutive
-				# frames where a given object has been marked as
-				# missing, deregister it
 				if self.disappeared[objectID] > self.maxDisappeared:
 					self.deregister(objectID)
 
